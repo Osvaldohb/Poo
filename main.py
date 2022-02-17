d8442712d807f067f65bcf2a7f7380b175e829e4
Osvaldohb <osvaldonuevo@gmail.com>
2022-02-17 11:20:41 -0600

    Se crea los objetos del curp

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 
+from datetime import date
+import datetime
 from random import randint
 
 
 class Person:
-    def __init__(self, name, curp, lastname, age, weight, height):
+    def __init__(self, name, curp, lastnameP,lastnameM, age, weight, height,state,fecha,sexo):
         self.name=name
         self.curp=curp
-        self.lastname=lastname
+        self.lastnameP=lastnameP
+        self.lastnameM=lastnameM
         self.age=age
         self.weight= weight
         self.height=height
-    #metodos: Birthday, dieta,crecer
-    def updateAge(self):
-        print("A que edad es a la cual deseas actualizar?")
-        self.age=input()
-        print("Esta es tu nueva edad: ",self.age)
-    
-    def ponerseADieta(self):
-        print("El sujeto en cuestion a decidido ponerse a dieta....")
-        print("Cual es la meta a bajar de peso?..")
-        self.weight=input()
-        print("Se pone a hacer ejercicio")
-        print("Dieta durante 1 mes...")
-        print("El nuevo peso es de ",self.weight)
-    def Crecer(self):
-        print("El sujeto comienza a crecer unos pocos centimetros..")
-        newhw=randint(1,4)/10
-        self.height=self.height+newhw
-        print(self.height)
+        self.state=state
+        self.fecha=fecha
+        self.sexo=sexo
+      
+    def lel(self):
+        curpNew=self.lastnameP[0:2]+self.lastnameM[0:1]+self.name[0:1]+self.fecha[8:11]+self.fecha[3:5]+self.fecha[0:2]+self.sexo[0:1]+self.state[0:2]
+        print (curpNew)
+        self.curp=curpNew
 
-Person1=Person("Osvaldo","SAJO030305HHGNMSA1","Santillan",18,74,1.80)
+padre=Person("Osvaldo","SAJO030305HHGNMSA1","SANTILLAN","Jimenez",18,74,1.80,"Hidalgo","05/03/2003","H")
+madre=Person("Maria","MASL040925HHGNMHA7","Gozanles","Perez",17,60, 1.75,"Hidalgo", "25/09/2004","M")
 
-print("¿Que opcion deceas elgegir?")
-print("1.----Actualizar edad------")
-print("2.----Empezar dieta--------")
-print("3.----Crecer---------------")
-lel=int(input("Digita tu eleccion: "))
 
-if lel==1:
-   Person1.updateAge()
-elif lel==2:
-    Person1.ponerseADieta()
-elif lel==3:
-    Person1.Crecer()
+print("Hola nos da mucho gusto que tengan un nuevo hijo, este programa se encargara de dar su curp al igual que proporcionar datos importantes")
+nom=input("Ingresa su Nombre: ")
+print("Ingresa su fecha de nacimiento con el formato DD/MM/YYYY")
+dia=int(input("Dia: "))
+mes=int(input("Mes: "))
+año=int(input("Año: "))
+fecha=datetime.datetime(año,mes,dia)
+now=datetime.datetime.now()
+conver1=datetime.datetime.strftime(now,'%Y')
+
+años=int(conver1)-año
+print(años)
+
+#print(fecha)
+fecha1=datetime.datetime.strftime(fecha,'%d/%m/%Y')
+print(fecha1)
+
+peso=input("Ingresa el peso del BB: ")
+altura=input("Ingresa la estatura del BB: ")
+estadoN=input("Ingresa estado de nacimiento: ")
+sexo=input("Ingresa el sexo H/M: ")
+
+
+hijo=Person(nom," ",padre.lastnameP,madre.lastnameM,años,peso,altura,estadoN,fecha1,sexo)
+
+
+hijo.lel()
 
 
 #Prueba de envio de datos por git     
